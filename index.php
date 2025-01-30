cec602e67ed3eb34a08bb6ff38a557cd7779aa6f
Stephen <stephen.dasilva@live.fr>
2025-01-30 14:47:10 +0100

    Création des templates header + footer, création du tableau multidimensionnel articles + boucle foreach

--- index.php
+++ index.php
@@ -1,41 +1,43 @@
-<!DOCTYPE html>
-<html lang="fr">
+<?php 
+  require_once __DIR__ . "/template/_header.php";
+  
+  $articles = [
+    ["title" => "Php VS Python", "content" => "Test", "image" => "1-php-vs-python.jpg"],
+    ["title" => "React ou React Native ?", "content" => "Test", "image" => "2-react-vs-react-native.jpg"],
+    ["title" => "Les meilleurs outils devops", "content" => "Test", "image" => "3-devops.png"],
+  ];
+  
+?>
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible"content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>TechTrendz</title>
-
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="stylesheet" href="assets/css/override-bootstrap.css">
-</head>
-
-<body>
-<div class="container">
-    <header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
-      <div class="col-md-3 mb-2 mb-md-0">
-        <a href="/" class="d-inline-flex link-body-emphasis text-decoration-none">
-          <img src="assets/images/logo-tech-trendz.png" alt="logo TechTrendz"width="150">
-        </a>
-      </div>
-
-      <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
-        <li><a href="#" class="nav-link px-2 link-secondary">Home</a></li>
-        <li><a href="#" class="nav-link px-2">Features</a></li>
-        <li><a href="#" class="nav-link px-2">Pricing</a></li>
-        <li><a href="#" class="nav-link px-2">FAQs</a></li>
-        <li><a href="#" class="nav-link px-2">About</a></li>
-      </ul>
-
-      <div class="col-md-3 text-end">
-        <button type="button" class="btn btn-outline-primary me-2">Login</button>
-        <button type="button" class="btn btn-primary">Sign-up</button>
-      </div>
-    </header>
+<div class="row flex-lg-row-reverse align-items-center g-5 py-5">
+  <div class="col-10 col-sm-8 col-lg-6">
+    <img src="assets/images/logo-tech-trendz.png" class="d-block mx-lg-auto img-fluid" alt="Logo TechTrendz" width="700" height="500" loading="lazy">
   </div>
+  <div class="col-lg-6">
+    <h1 class="display-5 fw-bold text-body-emphasis lh-1 mb-3">Responsive left-aligned hero with image</h1>
+    <p class="lead">Quickly design and customize responsive mobile-first sites with Bootstrap, the world’s most popular front-end open source toolkit, featuring Sass variables and mixins, responsive grid system, extensive prebuilt components, and powerful JavaScript plugins.</p>
+    <div class="d-grid gap-2 d-md-flex justify-content-md-start">
+      <a href="actualites.php" class="btn btn-primary btn-lg px-4 me-md-2">Voir toutes actualités</a>
+    </div>
+  </div>
+</div>
 
+<div class="row text-center">
+  <?php foreach($articles as $key=>$article) { ?>
+  <div class="col-md-4 my-2">
+    <div class="card">
+      <img src="uploads/articles/<?=$article['image'] ?>" class="card-img-top" alt="<?=$article['title'] ?>">
+      <div class="card-body">
+        <h5 class="card-title"><?=$article['title'] ?></h5>
+        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        <a href="#" class="btn btn-primary">Acceder à l'article</a>
+      </div>
+    </div>
+  </div>
+  <?php } ?>
 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-</body>
-</html>
+</div>
+      
+<?php 
+  require_once __DIR__ . "/template/_footer.php";
+?>
