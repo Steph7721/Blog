ff1622a3b6d99ad32b00f74924440df782a18fdf
Stephen <stephen.dasilva@live.fr>
2025-01-31 13:26:38 +0100

    Ajout du htmlentities pour la protection des failles xss

--- template/_header.php
+++ template/_header.php
@@ -11,8 +11,8 @@ $currentPage = basename($_SERVER["SCRIPT_NAME"]);
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible"content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="<?=$mainMenu[$currentPage]["meta_description"]?>">
-    <title><?=$mainMenu[$currentPage]["head_title"] ?></title>
+    <meta name="description" content="<?=htmlentities($mainMenu[$currentPage]["meta_description"])?>">
+    <title><?=htmlentities($mainMenu[$currentPage]["head_title"]) ?></title>
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="assets/css/override-bootstrap.css">
@@ -31,12 +31,12 @@ $currentPage = basename($_SERVER["SCRIPT_NAME"]);
           <?php foreach ($mainMenu as $key => $menuItem) {
             if(!array_key_exists("exclude", $menuItem)) {
             ?>
-            <li class="nav-item"><a href="<?=$key; ?>" class="nav-link px-2 <?php
+            <li class="nav-item"><a href="<?=htmlentities($key); ?>" class="nav-link px-2 <?php
               if($currentPage === $key){echo "active";}
               //Ceci est une autre façon de faire de la condition ternaire ci-dessus
               //echo $currentPage === $key ? "active" : "";
 
-              ?>"><?=$menuItem["title"]; ?></a></li>
+              ?>"><?=htmlentities($menuItem["title"]); ?></a></li>
           <?php }
             }
           ?>
