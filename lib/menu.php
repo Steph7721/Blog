2dc7c6bc187d94c52a4ed0bf320c3138974fd42e
Stephen <stephen.dasilva@live.fr>
2025-01-31 12:43:38 +0100

    modification de la fonction if

--- lib/menu.php
+++ lib/menu.php
@@ -1,9 +1,9 @@
 <?php
 
 $mainMenu = [
-  "index.php" => ["title" => "Accueil", "head_title" => "Accueil TechTrendz", "meta_description" => "TechTrendz, le site de référence pour les actualités tech !", "exclude" => false],
-  "actualites.php" => ["title" => "Actualités", "head_title" => "Actualités TechTrendz", "meta_description" => "Découvrez les dernières actualités tech !", "exclude" => false],
-  "a_propos.php" => ["title" => "A propos", "head_title" => "A propos de TechTrendz", "meta_description" => "En savoir plus sur TechTrendz", "exclude" => false],
+  "index.php" => ["title" => "Accueil", "head_title" => "Accueil TechTrendz", "meta_description" => "TechTrendz, le site de référence pour les actualités tech !"],
+  "actualites.php" => ["title" => "Actualités", "head_title" => "Actualités TechTrendz", "meta_description" => "Découvrez les dernières actualités tech !"],
+  "a_propos.php" => ["title" => "A propos", "head_title" => "A propos de TechTrendz", "meta_description" => "En savoir plus sur TechTrendz"],
 ];
 
 ?>
